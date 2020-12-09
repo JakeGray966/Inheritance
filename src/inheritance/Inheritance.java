6e93a30bbfedd3d2b46b87307ac4467997329405
Linkadd <34799196+linkadd515@users.noreply.github.com>
2020-12-09 19:38:42 +0000

    l
    l

---
+++ src/inheritance/Inheritance.java
@@ -0,0 +1,14 @@
+
+package inheritance;
+
+
+public class Inheritance {
+
+    public static void main(String[] args){
+        
+        Person Bruh = new Person ("Ya" , "Yeet", 4.2 , 19, "01166");
+        
+        System.out.println(Bruh);
+    }
+    
+}
